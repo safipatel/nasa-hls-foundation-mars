e003c9b946a8a63c5ee7a32449914e8fa1799459
Safi Patel <safipatel1@gmail.com>
2024-05-11 21:27:34 -0400

    updated dataloader code

--- classification_torch.py
+++ classification_torch.py
@@ -76,8 +76,8 @@ class PILToNumpyReflct(object):
         self.conversion_means = means
         self.conversion_stds = stds
 
-    def __call__(self, pil_img):
-        img = np.asarray( pil_img, dtype="uint16" )
+    def __call__(self, pil_img_label):
+        img = np.asarray( pil_img_label[0], dtype="uint16" )
         img = img.transpose(2,0,1)[[2,1,0]] # Put channels first and convert to BGR from RGB
 
         img[0,:]  = img[0,:] * ((self.conversion_means[0] + self.conversion_stds[0] * 2) / 255)
